7cfcdd228b0d540608fc3f311718b90cc3cfbb59
Sargis <saco.hakobyan@gmail.com>
2016-11-30 22:05:16 +0400

    Added list size and append operations

--- js/higher_order_functions.js
+++ js/higher_order_functions.js
@@ -1,9 +1,27 @@
 function FP(){
-	this.reduce = function(f, z, list) {
+	this.foldl = function(f, z, list) {
 		if(!list || list.length == 0)
-		 return z;
+			return z;
 
-		return this.reduce(f, f(z, list[0]), list.slice(1));
+		return this.foldl(f, f(z, list[0]), list.slice(1));
+	};
+	
+	this.foldr = function(f, z, list) {		   
+		if(!list || list.length == 0)
+			return z;
+		
+		return f(list[0], this.foldr(f, z, list.slice(1)));
+    };
+	
+	this.append = function(a, b) {
+		return fp.foldr(fp.cons, b, a)
+	};
+    this.cons = function(x, list) {
+		list.unshift(x);
+		return list;
+	};
+	this.size = function(x, y) {
+		return 1 + y;
 	};
 	
 	this.add = function(x, y) {
