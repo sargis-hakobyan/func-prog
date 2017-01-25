59de49283335bb9758db72fb86389a53e07eeda8
sargis-hakobyan <saco.hakobyan@gmail.com>
2017-01-25 19:38:37 +0400

    Added missing comments

--- js/higher_order_functions.js
+++ js/higher_order_functions.js
@@ -1,5 +1,13 @@
 function FP() {
-
+	
+	/**
+	 * @param f - function to call on list elements
+	 * @param z - accumulator
+	 * @param list - list to process
+	 * if the list is empty, returns accumulator
+	 * if the list is not empty: calls f function on accumulator and the first element of the list 
+	 * and the  result is the new accumulator for the next recursive call.
+	*/
 	this.foldl = function (f, z, list) {
 		if (!list || list.length == 0)
 			return z;
@@ -7,6 +15,13 @@ function FP() {
 		return this.foldl(f, f(z, list[0]), list.slice(1));
 	};
 
+	/**
+	 * @param f - function to call on list elements
+	 * @param z - accumulator
+	 * @param list - list to process
+	 * if the list is empty, returns accumulator
+	 * if the list is not empty: calls f function with the list’s first element and itself 
+	*/
 	this.foldr = function (f, z, list) {
 		if (!list || list.length == 0)
 			return z;
@@ -15,6 +30,8 @@ function FP() {
 	};
 
 	/** 
+	 * @param a - list to append
+	 * @param b - list to append
 	 * Appends two lists
 	 * a = [1,2,3], b = [a,b,c] -> result = [1,2,3,a,b,c] 
 	 * uses foldr(fold right) and cons functions
@@ -29,7 +46,10 @@ function FP() {
 		return fp.foldr(cons, b, a)
 	};
 
-	/** Returns elements that have even index */
+	/** 
+	 * @param list - list to process
+	 *Returns elements that have even index 
+	*/
 	this.getOddElements = function (list) {
 
 		/** Returns accumuliator which contains filtered list and boolean variable */
@@ -43,7 +63,11 @@ function FP() {
 		return result.list;
 	}
 
-	/** Processes given f function for each element of list */
+	/** 
+	 * @param f - function to apply on every elemnt of the list
+	 * @param list - list to process
+	 * Processes given f function for each element of the list 
+	*/
 	this.map = function(f, list) {
 		var cons = function(list, x) {
 			list.push(f(x));
